287ec19eab6791179db8cc903f0fb3226bb2f235
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2024-02-24 15:39:16 -0600

    Agregar archivos de proyecto.

---
+++ Carlos_Castaneda_LabPractico1WebApi/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Carlos_Castaneda_LabPractico1WebApi.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
